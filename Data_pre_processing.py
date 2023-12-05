d8a539f26ad534453ea3ac675a88dc7042633c8a
yuuhi <yuuhi.nakajima.138@gmail.com>
2023-12-05 15:12:10 +0900

    空き時間を求める関数を作成した

---
+++ Data_pre_processing.py
@@ -0,0 +1,56 @@
+#予定を用意する
+schedule_list = [['学校',7,17],['就寝,朝',0,7],['風呂,夕食',19,20],['就寝,夜',22,24]]
+
+#予定から、空き時間を求める
+default_free_time_list = [[0,24]]
+
+'''
+空き時間が、4時～6時、18時～20時だとすると、
+[ [4,6], [18,20] ] とする
+'''
+
+#空き時間のリストを作る
+#空き時間のリストを編集する関数
+
+def find_freetime(schedule,time_list):
+    
+    for sche in schedule:
+        b = fill_schedule(sche,time_list)
+        #変更後のリストを追加
+        for i in b[0]:
+            time_list.append(i)
+        #変更前のリストを削除
+        time_list.remove(b[1])
+
+    return time_list
+
+    
+#スケジュールから空き時間リストの変更箇所を求める
+def fill_schedule(schedule,free_time):
+    new_freetime = []
+    time_stock = []
+    for time in free_time:
+        #とりあえず、動く前提で分岐する
+        #print(time)
+        #print(schedule)
+        if(time[0] <= schedule[1]   and schedule[2] <= time[1]):
+            #予定を反映して、変更する部分の空き時間を求める
+            if(time[0] < schedule[1]):
+                new_freetime.append([time[0], schedule[1]])
+            
+            if(schedule[2] < time[1]):
+                new_freetime.append([schedule[2], time[1]])
+            time_stock = time
+            break
+
+    return [new_freetime,time_stock]
+
+#使い方
+default_free_time_list = find_freetime(schedule_list,default_free_time_list)
+
+print(default_free_time_list)
+'''
+for sche in schedule:
+    fill_schedule(sche)
+
+'''
--- graph.py
+++ graph.py
@@ -5,10 +5,16 @@ import seaborn as sns
 import japanize_matplotlib
 
 sns.set(font='IPAexGothic')
-
+'''
 dataset = pd.DataFrame([[100, 200, 50], [300, 400, 600], [50, 300, 60]], #タスクのその日の割り当て数に変更する
                        columns=['A支店', 'B支店', 'C支店'], #dateに変更する
                        index=['4月', '5月', '6月'])#task_nameに変更する
+
+'''
+dataset = pd.DataFrame([[1, 2, 1], [3, 1, 1], [1, 1, 1]], #タスクのその日の割り当て数に変更する 0の時に表示がおかしくなる 表示が真ん中じゃない時がある
+                       columns=['12月1日', '12月2日', '12月3日'], #dateに変更する
+                       index=['課題1', '課題2', '課題3'])#task_nameに変更する
+
 
 fig, ax = plt.subplots(figsize=(10, 8))
 for i in range(len(dataset)):
@@ -18,8 +24,17 @@ for i in range(len(dataset)):
                  y=dataset.iloc[:i, j].sum() + (dataset.iloc[i, j] / 2), 
                  s=dataset.iloc[i, j], 
                  ha='center', 
-                 va='bottom'
+                 va='center'
                 )
-ax.set(xlabel='支店名', ylabel='売り上げ')
+ax.set(xlabel='日付', ylabel='実施時間')
 ax.legend(dataset.index)
 plt.show()
+
+#使ったサンプルコード
+#https://qiita.com/s_fukuzawa/items/6f9c1a3d4c4f98ae6eb1ju
+
+
+#plt.textの内容
+#https://www.yutaka-note.com/entry/2020/01/08/080413#%E6%B0%B4%E5%B9%B3%E6%96%B9%E5%90%91%E3%81%AE%E6%96%87%E5%AD%97%E4%BD%8D%E7%BD%AEhorizontalalignment-%E3%81%BE%E3%81%9F%E3%81%AF-ha
+#棒グラフ全般
+#https://pythondatascience.plavox.info/matplotlib/%E6%A3%92%E3%82%B0%E3%83%A9%E3%83%95
